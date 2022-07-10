1a47a7006de1f4bb50467398381fe769a367764b
Amarendra Singh Yadav <82771352+Amarendra-ai@users.noreply.github.com>
2022-07-11 01:28:51 +0530

    Update neural_net.py

--- neural_net.py
+++ neural_net.py
@@ -14,11 +14,12 @@ test_images=test_images/255.0
 
 model = keras.Sequential ([
     keras.layers.Flatten(input_shape=(28,28)),
-    keras.layers.Dense(128,activation="relu"),
-    keras.layers.Dense(10,activation="softmax")
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
 ])
 
-model.compile(optimizer="adam", Loss="sparse_categorical_crossentropy",metrics=["accuracy"])
+model.compile(optimizer= keras.optimizers.Adam(), 
+loss= [keras.losses.SparseCategoricalCrossentropy(from_logits= True)], metrics=["accuracy"])
 
 model.fit(train_images,train_labels,epochs=5)
 
