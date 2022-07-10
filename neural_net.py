25a3a82f57eebd04601ff941a8cee99e42c3d67e
Amarendra Singh Yadav <82771352+Amarendra-ai@users.noreply.github.com>
2022-07-11 04:40:31 +0530

    Update neural_net.py

--- neural_net.py
+++ neural_net.py
@@ -15,15 +15,26 @@ test_images=test_images/255.0
 model = keras.Sequential ([
     keras.layers.Flatten(input_shape=(28,28)),
     keras.layers.Dense(128, activation="relu"),
-    keras.layers.Dense(10, activation="softmax")
+    keras.layers.Dense(128, activation="softmax")
 ])
 
 model.compile(optimizer= keras.optimizers.Adam(), 
-loss= [keras.losses.SparseCategoricalCrossentropy(from_logits= True)], metrics=["accuracy"])
+loss= [keras.losses.SparseCategoricalCrossentropy()], metrics=["accuracy"])
 
-model.fit(train_images,train_labels,epochs=5)
+model.fit(train_images,train_labels,epochs=5, verbose=2)
+
+
+prediction = model.predict(test_images)
+
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i],cmap=plt.cm.binary)
+    test_labels[i]
+    str(test_labels[i])
+
+    plt.xlabel("actual: " + str(test_labels[i]))
+    plt.title("prediction: " + class_names[np.argmax(prediction[i])])
+    plt.show()
 
 test_loss, test_acc = model.evaluate(test_images,test_labels)
-
 print("tested accuracy:",test_acc)
-
